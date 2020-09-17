f04c3d309cf7ad09e990a90e12c4ef03d3eef431
James Lamb <jameslamb13@gmail.com>
2020-09-16 22:33:02 -0400

    google maps is active on webpage.

--- script.js
+++ script.js
@@ -1,49 +1,63 @@
-$(document).ready(function(){
-    
+// Keep This Here
+googleApi = "https://maps.googleapis.com/maps/api/js?key=AIzaSyB-WOeEFR0l5fEi4fiug6nt43CVRRbqdc0&callback=initMap" 
 
+$("#google").attr("src", googleApi);
+// insert brewery name
+var breweryName = "The Stoogatz"
+// insert lat and lon from API
+var lat = 39.9526
+var lng = -75.1652
 
-
-var APIKey = "AIzaSyB-WOeEFR0l5fEi4fiug6nt43CVRRbqdc0"
-
-var script = document.createElement('script');
-
-script.src = "https://maps.googleapis.com/maps/api/js?key=" + APIKey + "&callback=" + initMap ;
-
-script.defer = true;
-
-document.head.appendChild(script);
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: {
-        lat: -34.397,
-        lng: 150.644
-      },
-      zoom: 10
-    });
-  }
+  function initMap(){
+      var options = {
+          zoom: 14,
+          center: {lat: lat, lng: lng}
+      }
+          var map = new google.maps.Map(document.getElementById('map'), options)
+  
  
+var marker = new google.maps.Marker({
+  position : { lat: lat, lng: lng},
+  map : map,
+  icon:'https://cdn.vox-cdn.com/thumbor/rgnZj-wJtFBWeGIq4beR04GU-8M=/200x200/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/19700731/googlemaps.png'
 });
 
+var infoWindow = new google.maps.InfoWindow({
+  content: '<h1>' + breweryName + '</h1>'
+});
+
+marker.addListener("click", function(){
+  infoWindow.open(map, marker);
+});
+
+  }
 
 
-   
-   
 
-   
-   
-   
-   
-   
-    // $("#type").on("click", function() {
-    //   var city = "New York"
-        
-    //     googleApi = "AIzaSyB-WOeEFR0l5fEi4fiug6nt43CVRRbqdc0"
-    //     var googleURL = "https://www.google.com/maps/embed/v1/search?key=" + googleApi + "&q=breweries+in+" + city;
-        
+
+
+
+
+
+
+
+
+//   var options = {
+//   center : {lat:42.3601, lng:-71.0589},
+//   zoom : 10
+
+// }
+//  map = new google.maps.Map($("map"), options)
+// // var marker = new google.maps.Marker({
+// //   position : options,
+// //   map : map
+// // })
        
 
-        
+// var APIKey = "AIzaSyB-WOeEFR0l5fEi4fiug6nt43CVRRbqdc0"
+
+;
+
         
         
         
