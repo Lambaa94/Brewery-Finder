089383c53bf0d5bf650656d125caaef3143ff53c
James Lamb <jameslamb13@gmail.com>
2020-09-17 20:05:01 -0400

    data types saving into local storage.

--- script.js
+++ script.js
@@ -6,31 +6,32 @@ $(document).ready(function(){
         city = $("#city").val();
         breweryType = $("#brewery-type").val();
         price = $("#price").val();
-        console.log(city)
-        console.log(breweryType)
-        console.log(price)
+        
         var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_type=" + breweryType
-        console.log(queryURL)
+        
         //OPEN BREWERY DB CALL
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
-            console.log(response)
+            
             //Test brewery is just first brewery on the list in this city (temporary)
             
             for(var i = 0; i < response.length; i++){
               var testBrewery = response[i].name;
               var newBreweryButton = $("<button>");
+              var hr = $("<hr>")
               newBreweryButton.text(testBrewery);
               newBreweryButton.attr("data-name", testBrewery);
-              $(".brewNames").append(newBreweryButton);
+              $(".brewNames").append(newBreweryButton).append(hr);
+              
+              
             
           }
-          
+            
            
             //YELP CALL BASED ON CITY VAR + TESTBREWERY VAR (TEMPORARY)
-            var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + testBrewery + "&location=" + city
+            var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + localStorage.getItem("Brewery Name") + "&location=" + city
         
             $.ajax({
                 url: yelpURL,
@@ -45,8 +46,8 @@ $(document).ready(function(){
                 console.log(yelp.businesses[0].location.address1)
                 var businessID = yelp.businesses[0].id
                 console.log(businessID)
-                lon = yelp[0].longitude;
-                lat = yelp[0].latitude;
+                lon = yelp[0].businesses.coordinates.longitude;
+                lat = yelp[0].businesses.coordinates.latitude;
                 //YELP RICH BUSINESS INFO CALL. Includes pics and more information. 
                 //Biz ID can also be used for another API call for user reviews if we're interested in that.
                 var yelpBizURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + businessID
@@ -90,8 +91,8 @@ $(document).ready(function(){
 
 })
 
-$(document).on("click", ".brewNames", function (){
-  console.log("clicked")
+$(document).on("click", "button", function (){
+
   var breweryTitle = $(this).attr("data-name");
   localStorage.setItem("Brewery Name", breweryTitle)
   console.log(breweryTitle)
@@ -104,8 +105,8 @@ $("#google").attr("src", googleApi);
 // insert brewery name
 var breweryName = "The Stoogatz"
 // insert lat and lon from API
-var lat = ""
-var lng = ""
+var lat;
+var lng;
 
   function initMap(){
       var options = {
